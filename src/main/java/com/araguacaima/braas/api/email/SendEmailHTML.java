7d713c94dcead76e5d04d8e2ddd02a90060697b9
Alejandro Manuel Méndez Araguacaima <araguacaima@gmail.com>
2019-10-08 15:55:42 -0500

    Adding mail sending feature

--- src/main/java/com/araguacaima/braas/api/email/SendEmailHTML.java
+++ src/main/java/com/araguacaima/braas/api/email/SendEmailHTML.java
@@ -1,25 +1,23 @@
 package com.araguacaima.braas.api.email;
 
-import com.araguacaima.braas.api.Server;
 import com.araguacaima.braas.core.MessageType;
 import com.sun.mail.smtp.SMTPTransport;
 import de.neuland.jade4j.Jade4J;
 import org.apache.commons.lang3.StringUtils;
 
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import java.io.*;
+import javax.mail.internet.MimeMultipart;
+import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 public class SendEmailHTML extends SendEmail {
     private static final String TEXT_HTML = "text/html";
@@ -27,7 +25,7 @@ public class SendEmailHTML extends SendEmail {
     private final String username;
     private final String password;
     private final boolean starttls;
-    private final String imageResourceName = Server.basePath + "/img/braas_116x116.png";
+    private final String imageResourcePath = SendEmailHTML.class.getClassLoader().getResource("./web/public/img/braas_116x116.png").getFile();
     private final String emailJadeTemplate = "web/views/email-template.jade";
     private final Properties properties;
     private String templateFile;
@@ -64,6 +62,7 @@ public class SendEmailHTML extends SendEmail {
         model.put("messageTypeError", MessageType.ERROR);
         model.put("messageTypeInfo", MessageType.INFO);
         model.put("title", subject);
+        String imageResourceName = UUID.randomUUID().toString();
         model.put("imageResourceName", imageResourceName);
 
 
@@ -78,7 +77,25 @@ public class SendEmailHTML extends SendEmail {
             msg.setFrom(new InternetAddress(from));
             msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientsList, false));
             msg.setSubject(subject);
-            msg.setDataHandler(new DataHandler(new HTMLDataSource(htmlContent)));
+            msg.setSentDate(new Date());
+
+            // creates message part
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent(htmlContent, TEXT_HTML);
+
+            // creates multi-part
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart);
+
+            // adds inline image attachments
+            MimeBodyPart imagePart = new MimeBodyPart();
+            imagePart.setHeader("Content-ID", "<" + imageResourceName + ">");
+            imagePart.setDisposition(MimeBodyPart.INLINE);
+            imagePart.attachFile(imageResourcePath);
+            multipart.addBodyPart(imagePart);
+
+            msg.setContent(multipart);
+
             SMTPTransport t = (SMTPTransport) session.getTransport("smtp");
             t.connect(host, username, password);
             t.sendMessage(msg, msg.getAllRecipients());
@@ -89,35 +106,5 @@ public class SendEmailHTML extends SendEmail {
             e.printStackTrace();
         }
         return response;
-    }
-
-    static class HTMLDataSource implements DataSource {
-
-        private String html;
-
-        public HTMLDataSource(String htmlString) {
-            html = htmlString;
-        }
-
-        @Override
-        public InputStream getInputStream() throws IOException {
-            if (html == null) throw new IOException("html message is null!");
-            return new ByteArrayInputStream(html.getBytes());
-        }
-
-        @Override
-        public OutputStream getOutputStream() throws IOException {
-            throw new IOException("This DataHandler cannot write HTML");
-        }
-
-        @Override
-        public String getContentType() {
-            return TEXT_HTML;
-        }
-
-        @Override
-        public String getName() {
-            return "HTMLDataSource";
-        }
     }
 }
